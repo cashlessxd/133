a1d383ab76f33c08a66a8486cca4ebf267cd6175
cashlessxd <mnoel8393@gmail.com>
2023-05-31 10:33:33 +0200

    implemented fetch beruf together with storage handling

--- projektarbeit/script.js
+++ projektarbeit/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
     $.getJSON("http://sandbox.gibm.ch/berufe.php")
         .done(function (data) {
-            $.each(data, function (key, filiale) {
-                $("#beruf").append("<option value='" + filiale.id  + "'>" + filiale.stadt + ", " + filiale.strasse + "</option>")
+            $.each(data, function (key, beruf) {
+                if (beruf.beruf_id == localStorage.getItem("selectedBerufId")) {
+                    $("#beruf").append("<option value='" + beruf.beruf_id  + "' selected>" + beruf.beruf_name + "</option>");
+                } else {
+                    $("#beruf").append("<option value='" + beruf.beruf_id  + "'>" + beruf.beruf_name + "</option>");
+                }
             })
         }).fail(function () {
         console.log("Request Failed");
     });
 })
+
+$("#beruf").on("change", function(){
+    localStorage.setItem("selectedBerufId", this.value); 
+});
